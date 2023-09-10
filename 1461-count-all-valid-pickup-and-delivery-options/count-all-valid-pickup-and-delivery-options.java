9b162918de2845e1db61207edc8ecf987fa8d344
Hemant Parihar <hemant.parihar264@gmail.com>
2023-09-10 22:25:27 +0530

    Time: 0 ms (100.00%) | Memory: 38.7 MB (99.39%) - LeetSync

--- 1461-count-all-valid-pickup-and-delivery-options/count-all-valid-pickup-and-delivery-options.java
+++ 1461-count-all-valid-pickup-and-delivery-options/count-all-valid-pickup-and-delivery-options.java
@@ -1,39 +1,15 @@
 class Solution {
-    int MOD = 1000000007;
+    int mod = 1000000007;
     public int countOrders(int n) {
-        long[][] dp = new long[n + 1][n + 1];
-        for(int i = 0; i <= n; i++) {
-            for(int j = 0; j <= n; j++) {
-                dp[i][j] = -1;
-            }
-        }
-        return (int)solve(n, 0, dp);
-    }
+        if (n == 1) return 1;
 
-    long solve(int pickUp, int delivery, long[][] dp) {
-        
-        if (dp[pickUp][delivery] != -1) {
-            return dp[pickUp][delivery];
-        }
+        long ans = (2*n - 1) * n;
 
-        if (pickUp == 0) return nFac(delivery);
+        long val = countOrders(n - 1);
 
-        long ans = 0;
-        ans = ( ans + pickUp * solve(pickUp - 1, delivery + 1, dp) ) % MOD;
-        if (delivery > 0) {
-            ans = ( ans + delivery * solve(pickUp, delivery - 1, dp) ) % MOD;
-        }
-        
-        dp[pickUp][delivery] = ans;
-        return ans;
-    }
+        ans *= val;
+        ans %= mod;
 
-    long nFac(int num) {
-        long ans = 1;
-        while(num > 1) {
-            ans = ( ans * num ) % MOD;
-            num--;
-        }
-        return ans;
+        return  (int)ans;
     }
 }
