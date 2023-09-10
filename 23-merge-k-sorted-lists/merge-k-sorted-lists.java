fc0c38f7ed2a840e4c853ad8172dfc6574855e8c
Hemant Parihar <hemant.parihar264@gmail.com>
2023-09-10 18:14:22 +0530

    Time: 1 ms (100.00%) | Memory: 43.8 MB (28.89%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.java
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.java
@@ -11,31 +11,63 @@
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
         int k = lists.length;
-        ListNode[] ptrArr = new ListNode[k];
-        ListNode ans = null;
-        ListNode head = null;
-        for(int i = 0; i < k; i++) {
-            ptrArr[i] = lists[i];
+        int intervals = 1;
+        while(intervals < k) {
+            for(int i = 0; i + intervals < k; i += 2 * intervals) {
+                // System.out.println("call " + i + " " + (i + intervals));
+                lists[i] = merge(lists[i], lists[i + intervals]);
+            }
+            intervals = intervals << 1;
         }
-        while(true) {
-            int min = Integer.MAX_VALUE;
-            int min_index = -1;
-            for(int i = 0; i < k; i++) {
-                if (ptrArr[i] != null && min > ptrArr[i].val) {
-                    min = ptrArr[i].val;
-                    min_index = i;
+        return k == 0 ? null : lists[0];
+    }
+
+    ListNode merge(ListNode l1, ListNode l2) {
+        ListNode head = null;
+        ListNode ptr = null;
+        while(l1 != null && l2 != null) {
+            // System.out.println(l1.val + " " + l2.val);
+            if (head == null) {
+                if (l1.val <= l2.val) {
+                    head = l1;
+                    l1 = l1.next;
+                    ptr = head;
+                } else {
+                    head = l2;
+                    l2 = l2.next;
+                    ptr = head;
+                }
+            } else {
+                if (l1.val <= l2.val) {
+                    ptr.next = l1;
+                    ptr = l1;
+                    l1 = l1.next;
+                } else {
+                    ptr.next = l2;
+                    ptr = l2;
+                    l2 = l2.next;
                 }
             }
-            if (min_index == -1) break;
-            if (ans == null) {
-                head = ptrArr[min_index];
-                ans = head;
-            } else {
-                ans.next = ptrArr[min_index];
-                ans = ptrArr[min_index];
-            }
-            ptrArr[min_index] = ptrArr[min_index].next;
         }
+
+        if (l1 != null) {
+            if (ptr == null) {
+                head = l1;
+            } else {
+                ptr.next = l1;
+            }
+        }
+
+        if (l2 != null) {
+            if (ptr == null) {
+                head = l2;
+            } else {
+                ptr.next = l2;
+            }
+        }
+
         return head;
     }
+
+
 }
