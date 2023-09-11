fb2cdd21684dbed030a29908ea6d952c57517b58
Hemant Parihar <hemant.parihar264@gmail.com>
2023-09-12 01:51:29 +0530

    Time: 60 ms (37.98%) | Memory: 56.7 MB (67.09%) - LeetSync

--- 1996-number-of-ways-to-rearrange-sticks-with-k-sticks-visible/number-of-ways-to-rearrange-sticks-with-k-sticks-visible.java
+++ 1996-number-of-ways-to-rearrange-sticks-with-k-sticks-visible/number-of-ways-to-rearrange-sticks-with-k-sticks-visible.java
@@ -1,9 +1,11 @@
 class Solution {
-    int mod = 1000000007;
+    int MOD = 1000000007;
     public int rearrangeSticks(int n, int k) {
-        long[][] dp = new long[n + 1][k + 1];
-        for(int i = 0; i <= n; i++) {
-            Arrays.fill(dp[i], - 1);
+        long[][] dp = new long[n+1][k+1];
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= i && j <= k; j++) {
+                dp[i][j] = -1;
+            }
         }
         return (int)solve(n, k, dp);
     }
@@ -12,15 +14,16 @@ class Solution {
         if (n == k) return 1;
         if (n < k) return 0;
         if (n < 1) return 0;
-        if (k < 0) return 0;
 
-        
-        if (dp[n][k] != -1) return dp[n][k];
+        if (dp[n][k] != -1) {
+            return dp[n][k];
+        }
 
         long ans = 0;
-        ans = (ans + solve(n -1, k - 1, dp)) % mod;
-        ans = (ans + (n -1) * solve(n - 1, k, dp)) % mod;
+        ans = (ans + solve(n - 1, k-1, dp));
+        ans = (ans + ((n-1) * solve(n-1, k, dp)) % MOD) % MOD;
 
-        return dp[n][k] = ans;
+        dp[n][k] = ans;
+        return ans;
     }
 }
