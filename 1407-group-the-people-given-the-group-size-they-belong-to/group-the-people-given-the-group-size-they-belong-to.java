838e2e8fb53e2bc16051db2fa521065acb42f51a
Hemant Parihar <hemant.parihar264@gmail.com>
2023-09-11 16:34:44 +0530

    Time: 5 ms (93.77%) | Memory: 44.5 MB (59.12%) - LeetSync

--- 1407-group-the-people-given-the-group-size-they-belong-to/group-the-people-given-the-group-size-they-belong-to.java
+++ 1407-group-the-people-given-the-group-size-they-belong-to/group-the-people-given-the-group-size-they-belong-to.java
@@ -1,33 +1,15 @@
 class Solution {
-    public List<List<Integer>> groupThePeople(int[] groupSizes) {
-        int n = groupSizes.length;
-        List<List<Integer>> ans = new ArrayList<>();
-        HashMap<Integer, Queue<Integer>> map = new HashMap<>();
-        for(int i = 0; i < n; i++) {
-            if (map.containsKey(groupSizes[i])) {
-                map.get(groupSizes[i]).add(i);
-            } else {
-                Queue<Integer> queue = new LinkedList<>();
-                queue.add(i);
-                map.put(groupSizes[i], queue);
+    public List<List<Integer>> groupThePeople(int[] gz) {
+        List<List<Integer>> res = new ArrayList();
+        Map<Integer, List<Integer>> groups = new HashMap<>();
+        for (int i = 0; i < gz.length; ++i) {
+            List<Integer> list = groups.computeIfAbsent(gz[i], k -> new ArrayList());
+            list.add(i);
+            if (list.size() == gz[i]) {
+            res.add(list);
+            groups.put(gz[i], new ArrayList());
             }
         }
-
-        for(Map.Entry<Integer, Queue<Integer>> entry : map.entrySet()) {
-            int key = entry.getKey();
-            Queue<Integer> queue = entry.getValue();
-            // System.out.println(queue);
-            List<Integer> list = new ArrayList<>();
-            int count = 0;
-            while(!queue.isEmpty()) {
-                list.add(queue.poll());
-                count++;
-                if (count % key == 0) {
-                    ans.add(new ArrayList<>(list));
-                    list.clear();
-                }
-            }
-        }
-        return ans;
+        return res;
     }
 }
