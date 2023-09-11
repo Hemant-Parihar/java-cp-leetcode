d29224aa90620f88cb9f8ca57293dc92ec23140f
Hemant Parihar <hemant.parihar264@gmail.com>
2023-09-11 16:33:01 +0530

    Time: 7 ms (55.15%) | Memory: 44.7 MB (20.16%) - LeetSync

--- 1407-group-the-people-given-the-group-size-they-belong-to/group-the-people-given-the-group-size-they-belong-to.java
+++ 1407-group-the-people-given-the-group-size-they-belong-to/group-the-people-given-the-group-size-they-belong-to.java
@@ -2,21 +2,31 @@ class Solution {
     public List<List<Integer>> groupThePeople(int[] groupSizes) {
         int n = groupSizes.length;
         List<List<Integer>> ans = new ArrayList<>();
+        HashMap<Integer, Queue<Integer>> map = new HashMap<>();
         for(int i = 0; i < n; i++) {
-            if (groupSizes[i] == -1) continue;
+            if (map.containsKey(groupSizes[i])) {
+                map.get(groupSizes[i]).add(i);
+            } else {
+                Queue<Integer> queue = new LinkedList<>();
+                queue.add(i);
+                map.put(groupSizes[i], queue);
+            }
+        }
 
-
-            int val = groupSizes[i];
+        for(Map.Entry<Integer, Queue<Integer>> entry : map.entrySet()) {
+            int key = entry.getKey();
+            Queue<Integer> queue = entry.getValue();
+            // System.out.println(queue);
             List<Integer> list = new ArrayList<>();
             int count = 0;
-            for(int j = i; j < n && count < val; j++) {
-                if (groupSizes[j] == val) {
-                    count++;
-                    groupSizes[j] = -1;
-                    list.add(j);
+            while(!queue.isEmpty()) {
+                list.add(queue.poll());
+                count++;
+                if (count % key == 0) {
+                    ans.add(new ArrayList<>(list));
+                    list.clear();
                 }
             }
-            ans.add(list);
         }
         return ans;
     }
