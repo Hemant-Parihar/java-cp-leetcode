0c2ef823f9a8c2ecb40770112cbc30939880e8ef
Hemant Parihar <hemant.parihar264@gmail.com>
2023-09-12 01:43:12 +0530

    Time: 109 ms (17.72%) | Memory: 56.8 MB (64.56%) - LeetSync

---
+++ 1996-number-of-ways-to-rearrange-sticks-with-k-sticks-visible/number-of-ways-to-rearrange-sticks-with-k-sticks-visible.java
@@ -0,0 +1,23 @@
+class Solution {
+    int mod = 1000000007;
+    public int rearrangeSticks(int n, int k) {
+        long[][] dp = new long[n + 1][k + 1];
+        for(int i = 0; i <= n; i++) {
+            Arrays.fill(dp[i], - 1);
+        }
+        return (int)solve(n, k, dp);
+    }
+
+    long solve(int n, int k, long[][] dp) {
+        if (n == 0 && k == 0) return 1;
+        if (n > 0 && k == 0 || n == 0 && k > 0) return 0;
+        
+        if (dp[n][k] != -1) return dp[n][k];
+
+        long ans = 0;
+        ans = (ans + solve(n -1, k - 1, dp)) % mod;
+        ans = (ans + (n -1) * solve(n - 1, k, dp)) % mod;
+
+        return dp[n][k] = ans;
+    }
+}
