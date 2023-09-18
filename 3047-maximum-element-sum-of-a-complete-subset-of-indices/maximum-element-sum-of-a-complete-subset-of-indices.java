dea872e089e4783c8410e3b11f97cd96bdfbb202
Hemant Parihar <hemant.parihar264@gmail.com>
2023-09-18 16:26:37 +0530

    Time: 2335 ms (20.00%) | Memory: 44.9 MB (40.00%) - LeetSync

---
+++ 3047-maximum-element-sum-of-a-complete-subset-of-indices/maximum-element-sum-of-a-complete-subset-of-indices.java
@@ -0,0 +1,24 @@
+class Solution {
+    public long maximumSum(List<Integer> nums) {
+        int n = nums.size();
+        long ans = 0;
+
+        for(int i = 0; i < n; i++) {
+            int j = i + 1;
+            long temp_ans = nums.get(i);
+            while(j < n) {
+                int val = (i + 1) * (j + 1);
+                int sq_val = (int) Math.sqrt(val);
+                if (sq_val * sq_val == val) {
+                    // System.out.println( (i + 1) + " " + (j + 1));
+                    temp_ans += nums.get(j);
+                }
+                j++;
+            }
+            ans = Math.max(ans, temp_ans);
+            // System.out.println(temp_ans);
+        }
+
+        return ans;
+    }
+}
