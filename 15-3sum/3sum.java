4d2a36e95235de7000dd35c94bb95b1a34e5a6b0
Hemant Parihar <hemant.parihar264@gmail.com>
2023-10-04 22:37:00 +0530

    Time: 500 ms (9.05%) | Memory: 52.3 MB (8.28%) - LeetSync

---
+++ 15-3sum/3sum.java
@@ -0,0 +1,59 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        int n = nums.length;
+        Arrays.sort(nums);
+        HashMap<Integer, Integer> map = new HashMap<>();
+        HashSet<List<Integer>> set = new HashSet<>();
+
+        for(int i = 0; i < n; i++) {
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+        }
+
+        int prev_i = Integer.MIN_VALUE;
+
+        for(int i = 0; i < n; i++) {
+            // if (prev_i == nums[i]) {
+            //     continue;
+            // }
+
+            if (nums[i] > 0) {
+                break;
+            }
+
+            int prev_j = Integer.MIN_VALUE;
+
+            for(int j = i + 1; j < n; j++) {
+                // if (prev_j == nums[j]) {
+                //     continue;
+                // }
+                
+                if (nums[i] + nums[j] > 0) {
+                    break;
+                }
+
+                int val = - (nums[i] + nums[j]);
+                if (val < nums[j]) continue;
+
+                if (nums[i] == nums[j] && nums[j] == val) {
+                    if (map.get(val) >= 3) {
+                        set.add(new ArrayList<>(List.of(nums[i], nums[j], val)));
+                    }
+                } else if (nums[j] == val) {
+                    if (map.get(val) >= 2) {
+                        set.add(new ArrayList<>(List.of(nums[i], nums[j], val)));
+                    }
+                } else {
+                    if (map.containsKey(val)) {
+                        set.add(new ArrayList<>(List.of(nums[i], nums[j], val)));
+                    }
+                }
+
+                prev_j = nums[j];
+            }
+            prev_i = nums[i];
+        }
+
+        List<List<Integer>> list = new ArrayList<>(set);
+        return list;
+    }
+}
