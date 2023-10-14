d64a51f601996e940e239214f440513b7f5aa70e
Hemant Parihar <hemant.parihar264@gmail.com>
2023-10-14 22:55:28 +0530

    Time: 29 ms (42.12%) | Memory: 44.1 MB (80.95%) - LeetSync

--- 855-count-unique-characters-of-all-substrings-of-a-given-string/count-unique-characters-of-all-substrings-of-a-given-string.java
+++ 855-count-unique-characters-of-all-substrings-of-a-given-string/count-unique-characters-of-all-substrings-of-a-given-string.java
@@ -1,17 +1,15 @@
 class Solution {
     public int uniqueLetterString(String s) {
         int n = s.length();
-        Pair<Integer, Integer>[] pairList = new Pair[26];
-        for(int i = 0; i < 26; i++) {
-            pairList[i] = new Pair(0, 0);
-        }
+        int[][] pairList = new int[26][2];
+
         int res = 0;
         for(int i = 0; i < n; i++) {
             int index = s.charAt(i) - 'A';
-            Pair<Integer, Integer> newPair = new Pair(i + 1, pairList[index].getKey());
-            pairList[index] = newPair;
+            pairList[index][1] = pairList[index][0];
+            pairList[index][0] = i + 1;
             for(int j = 0; j < 26; j++) {
-                res += pairList[j].getKey() - pairList[j].getValue();
+                res += pairList[j][0] - pairList[j][1];
             }
         }
         return res;
