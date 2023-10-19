055473cdab874c68134ff3eba08e4d5309e7017c
Hemant Parihar <hemant.parihar264@gmail.com>
2023-10-20 00:17:15 +0530

    Time: 30 ms (52.38%) | Memory: 54.2 MB (82.06%) - LeetSync

---
+++ 359-logger-rate-limiter/logger-rate-limiter.java
@@ -0,0 +1,24 @@
+class Logger {
+
+    HashMap<String, Integer> map;
+    public Logger() {
+        map = new HashMap<>();
+    }
+    
+    public boolean shouldPrintMessage(int timestamp, String message) {
+        if (map.containsKey(message)) {
+            int time = map.get(message);
+            if (timestamp - time < 10) {
+                return false;
+            }
+        }
+        map.put(message, timestamp);
+        return true;
+    }
+}
+
+/**
+ * Your Logger object will be instantiated and called as such:
+ * Logger obj = new Logger();
+ * boolean param_1 = obj.shouldPrintMessage(timestamp,message);
+ */
