48e2df39b1600519e44472934c0658e348601801
Hemant Parihar <hemant.parihar264@gmail.com>
2024-09-08 01:27:54 +0530

    Time: 33 ms (21.31%) | Memory: 45.8 MB (9.11%) - LeetSync

---
+++ 322-coin-change/coin-change.java
@@ -0,0 +1,31 @@
+class Solution {
+
+    int MAX_NUM = 10001;
+    public int coinChange(int[] coins, int amount) {
+        int n = coins.length;
+        int[][] dp = new int[n][amount + 1];
+
+        for(int i = 0; i < n; i++) {
+            Arrays.fill(dp[i], -1);
+        }
+
+        int ans = solve(n - 1, coins, amount, dp);
+        if (ans >= MAX_NUM) return -1;
+        return ans;
+    }
+
+    int solve(int i, int[] coins, int amount, int[][] dp) {
+        if (amount < 0 || i < 0) return MAX_NUM;
+        if (amount == 0) return 0;
+
+        if (dp[i][amount] != -1) return dp[i][amount];
+
+        int p, up = Integer.MAX_VALUE;
+        if (amount > 0) {
+            up = 1 + solve(i, coins, amount - coins[i], dp);
+        }
+        p = solve(i - 1, coins, amount, dp);
+        dp[i][amount] = Math.min(p, up);
+        return dp[i][amount];
+    }
+}
