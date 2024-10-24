a4b8fec2eaaa0956b2b55e37f886fae7f37642fb
Hemant Parihar <hemant.parihar264@gmail.com>
2024-10-25 01:36:56 +0530

    Time: 2 ms (54.30%) | Memory: 44.9 MB (44.12%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.java
+++ 152-maximum-product-subarray/maximum-product-subarray.java
@@ -1,15 +1,45 @@
 class Solution {
     public int maxProduct(int[] nums) {
-        int n = nums.length;
-        int max = nums[0];
-        int min = nums[0];
-        int ans = nums[0];
-        for(int i = 1; i < n; i++) {
-            int temp_max = Math.max(nums[i], Math.max(max * nums[i], min * nums[i]));
-            min = Math.min(nums[i], Math.min(max * nums[i], min * nums[i]));
-            max = temp_max;
-            ans = Math.max(max, ans);
+        int g_prod = nums[0];
+        int prod = 1;
+        boolean flag = true;
+
+        if (nums.length == 1) {
+            return nums[0];
         }
-        return ans;
+
+        for(int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                prod *= nums[i];
+                flag = false;
+            } else {
+                prod = 1;
+                flag = true;
+            }
+            if (flag == false) {
+                g_prod = Math.max(g_prod, prod);
+            } else {
+                g_prod = Math.max(g_prod, 0);
+            }
+        }
+
+        prod = 1;
+        flag = true;
+        for(int i = nums.length - 1; i >= 0; i--) {
+            if (nums[i] != 0) {
+                prod *= nums[i];
+                flag = false;
+            } else {
+                prod = 1;
+                flag = true;
+            }
+            if (flag == false) {
+                g_prod = Math.max(g_prod, prod);
+            } else {
+                g_prod = Math.max(g_prod, 0);
+            }
+        }
+
+        return g_prod;
     }
 }
