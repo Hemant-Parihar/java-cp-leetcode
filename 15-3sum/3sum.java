e29aa6ea251604116d943e215c869bad4ccffc1b
Hemant Parihar <hemant.parihar264@gmail.com>
2024-10-26 15:25:03 +0530

    Time: 34 ms (37.51%) | Memory: 52.6 MB (18.44%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,58 +1,53 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        int n = nums.length;
         Arrays.sort(nums);
-        HashMap<Integer, Integer> map = new HashMap<>();
-        List<List<Integer>> ansList = new ArrayList<>();
+        List<List<Integer>> ans = new ArrayList<>();
 
-        for(int i = 0; i < n; i++) {
-            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+        // System.out.println(Arrays.toString(nums));
+
+        int prev = Integer.MIN_VALUE;
+        for(int i = 0; i < nums.length; i++) {
+            if (prev == nums[i]) continue;
+            int s = i + 1;
+            int e = nums.length - 1;
+            int target = 0 - nums[i];
+
+            while(s < e) {
+                if (nums[s] + nums[e] > target) {
+                    int temp = nums[e];
+                    e = e - 1;
+                    while (e - 1 > s && nums[e] == temp) {
+                        e = e - 1;
+                    }
+                } else if (nums[s] + nums[e] < target) {
+                    int temp = nums[s];
+                    s = s + 1;
+                    while(s + 1 < e && nums[s] == temp) {
+                        s = s + 1;
+                    }
+                } else if (s < e) {
+                    List<Integer> list = new ArrayList<>();
+                    // System.out.println(i + " " + s + " " + e);
+                    list.add(nums[i]);
+                    list.add(nums[s]);
+                    list.add(nums[e]);
+                    ans.add(list);
+
+                    int temp = nums[e];
+                    e = e - 1;
+                    while (e - 1 > s && nums[e] == temp) {
+                        e = e - 1;
+                    }
+
+                    temp = nums[s];
+                    s = s + 1;
+                    while(s + 1 < e && nums[s] == temp) {
+                        s = s + 1;
+                    }
+                }
+            }
+            prev = nums[i];
         }
-
-        int prev_i = Integer.MIN_VALUE;
-
-        for(int i = 0; i < n; i++) {
-            if (prev_i == nums[i]) {
-                continue;
-            }
-
-            if (nums[i] > 0) {
-                break;
-            }
-
-            int prev_j = Integer.MIN_VALUE;
-
-            for(int j = i + 1; j < n; j++) {
-                if (prev_j == nums[j]) {
-                    continue;
-                }
-
-                if (nums[i] + nums[j] > 0) {
-                    break;
-                }
-
-                int val = - (nums[i] + nums[j]);
-                if (val < nums[j]) continue;
-
-                if (nums[i] == nums[j] && nums[j] == val) {
-                    if (map.get(val) >= 3) {
-                        ansList.add(new ArrayList<>(List.of(nums[i], nums[j], val)));
-                    }
-                } else if (nums[j] == val) {
-                    if (map.get(val) >= 2) {
-                        ansList.add(new ArrayList<>(List.of(nums[i], nums[j], val)));
-                    }
-                } else {
-                    if (map.containsKey(val)) {
-                        ansList.add(new ArrayList<>(List.of(nums[i], nums[j], val)));
-                    }
-                }
-
-                prev_j = nums[j];
-            }
-            prev_i = nums[i];
-        }
-
-        return ansList;
+        return ans;
     }
 }
