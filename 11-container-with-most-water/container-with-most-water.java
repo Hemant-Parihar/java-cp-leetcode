33218571feacab49201a7161dca9157751b8f7c9
Hemant Parihar <hemant.parihar264@gmail.com>
2024-10-29 15:50:46 +0530

    Time: 1120 ms (8.28%) | Memory: 56.2 MB (99.46%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.java
+++ 11-container-with-most-water/container-with-most-water.java
@@ -1,17 +1,43 @@
 class Solution {
     public int maxArea(int[] height) {
         int n = height.length;
+        List<Pair<Integer, Integer>> front = new ArrayList<>();
+
+        front.add(new Pair(0, height[0]));
+
         int ans = 0;
-        for(int i = 0; i < n; i++) {
-            for(int j = n - 1; j > i; j--) {
-                if (height[j] >= height[i]) {
-                    ans = Math.max(ans, height[i] * (j - i));
-                    break;
-                } else {
-                    ans = Math.max(ans, height[j] * (j - i));
-                }
+        for(int i = 1; i < height.length; i++) {
+            int size = front.size();
+            for(int j = size - 1; j >= 0; j--) {
+                int index = front.get(j).getKey();
+
+                int temp_ans = (i - index ) * Math.min(height[i], height[index]);
+                ans = Math.max(ans, temp_ans);
+            }
+
+            if (front.get(size - 1).getValue() < height[i]) {
+                front.add(new Pair(i, height[i]));
             }
         }
+
+        List<Pair<Integer, Integer>> back = new ArrayList<>();
+
+        back.add(new Pair(n - 1, height[n - 1]));
+
+        for(int i = n - 2; i >= 0; i--) {
+            int size = back.size();
+            for(int j = size - 1; j >= 0; j--) {
+                int index = back.get(j).getKey();
+
+                int temp_ans = ( index  - i) * Math.min(height[i], height[index]);
+                ans = Math.max(ans, temp_ans);
+            }
+            
+            if (back.get(size - 1).getValue() < height[i]) {
+                back.add(new Pair(i, height[i]));
+            }   
+        }
+
         return ans;
     }
 }
