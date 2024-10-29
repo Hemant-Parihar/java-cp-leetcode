96a52c47958c9221abaf421c0a932b5ee09a4e05
Hemant Parihar <hemant.parihar264@gmail.com>
2024-10-29 15:56:50 +0530

    Time: 944 ms (8.28%) | Memory: 57.7 MB (60.43%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.java
+++ 11-container-with-most-water/container-with-most-water.java
@@ -1,40 +1,37 @@
 class Solution {
     public int maxArea(int[] height) {
         int n = height.length;
-        List<Pair<Integer, Integer>> front = new ArrayList<>();
+        List<Integer> front = new ArrayList<>();
 
-        front.add(new Pair(0, height[0]));
+        front.add(0);
 
         int ans = 0;
         for(int i = 1; i < height.length; i++) {
             int size = front.size();
             for(int j = size - 1; j >= 0; j--) {
-                int index = front.get(j).getKey();
-
+                int index = front.get(j);
                 int temp_ans = (i - index ) * Math.min(height[i], height[index]);
                 ans = Math.max(ans, temp_ans);
             }
-
-            if (front.get(size - 1).getValue() < height[i]) {
-                front.add(new Pair(i, height[i]));
+            if (height[front.get(size - 1)] < height[i]) {
+                front.add(i);
             }
         }
 
-        List<Pair<Integer, Integer>> back = new ArrayList<>();
+        List<Integer> back = new ArrayList<>();
 
-        back.add(new Pair(n - 1, height[n - 1]));
+        back.add(n - 1);
 
         for(int i = n - 2; i >= 0; i--) {
             int size = back.size();
             for(int j = size - 1; j >= 0; j--) {
-                int index = back.get(j).getKey();
-
+                int index = back.get(j);
                 int temp_ans = ( index  - i) * Math.min(height[i], height[index]);
                 ans = Math.max(ans, temp_ans);
             }
             
-            if (back.get(size - 1).getValue() < height[i]) {
-                back.add(new Pair(i, height[i]));
+            if (height[back.get(size - 1)] < height[i]) {
+                back.add(i);
             }   
         }
 
