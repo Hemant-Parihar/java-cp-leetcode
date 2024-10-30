f93d5cd6ce62f30f91368b6f070ebc45a6db650d
Hemant Parihar <hemant.parihar264@gmail.com>
2024-10-30 12:35:14 +0530

    Time: 5 ms (73.89%) | Memory: 57.9 MB (50.68%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.java
+++ 11-container-with-most-water/container-with-most-water.java
@@ -1,40 +1,20 @@
 class Solution {
     public int maxArea(int[] height) {
         int n = height.length;
-        List<Integer> front = new ArrayList<>();
-
-        front.add(0);
-
+        int start = 0;
+        int end = n - 1;
         int ans = 0;
-        for(int i = 1; i < height.length; i++) {
-            int size = front.size();
-            for(int j = size - 1; j >= 0; j--) {
-                int index = front.get(j);
-                int temp_ans = (i - index ) * Math.min(height[i], height[index]);
-                ans = Math.max(ans, temp_ans);
-            }
-            if (height[front.get(size - 1)] < height[i]) {
-                front.add(i);
+        int temp_ans;
+        while(start < end) {
+            temp_ans = (end - start) * Math.min(height[start], height[end]);
+            ans = Math.max(ans, temp_ans);
+
+            if (height[start] < height[end]) {
+                start++;
+            } else {
+                end--;
             }
         }
-
-        List<Integer> back = new ArrayList<>();
-
-        back.add(n - 1);
-
-        for(int i = n - 2; i >= 0; i--) {
-            int size = back.size();
-            for(int j = size - 1; j >= 0; j--) {
-                int index = back.get(j);
-                int temp_ans = ( index  - i) * Math.min(height[i], height[index]);
-                ans = Math.max(ans, temp_ans);
-            }
-            
-            if (height[back.get(size - 1)] < height[i]) {
-                back.add(i);
-            }   
-        }
-
         return ans;
     }
 }
