f63b957e0d14b02bbd5428f2f71a20847cf7b9ae
Hemant Parihar <hemant.parihar264@gmail.com>
2024-10-30 12:41:49 +0530

    Time: 1 ms (100.00%) | Memory: 45.8 MB (32.25%) - LeetSync

--- 55-jump-game/jump-game.java
+++ 55-jump-game/jump-game.java
@@ -1,13 +1,13 @@
 class Solution {
     public boolean canJump(int[] nums) {
         int n = nums.length;
-        int goal = n - 1;
+        int end_to_reach = n -1;
         for(int i = n - 2; i >= 0; i--) {
-            if (i + nums[i] >= goal) {
-                goal = i;
+            if (i + nums[i] >= end_to_reach) {
+                end_to_reach = i;
             }
         }
-        if (goal == 0) return true;
+        if (end_to_reach == 0) return true;
         return false;
     }
 }
