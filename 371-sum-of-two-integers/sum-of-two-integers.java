8a4cc466f68989da6b475986d0157a7d49a58779
Hemant Parihar <hemant.parihar264@gmail.com>
2024-11-07 19:50:20 +0530

    Time: 0 ms (100.00%) | Memory: 40.3 MB (51.53%) - LeetSync

--- 371-sum-of-two-integers/sum-of-two-integers.java
+++ 371-sum-of-two-integers/sum-of-two-integers.java
@@ -4,27 +4,31 @@ class Solution {
         int c = 0;
         for(int i = 0; i < 32; i++) {
             if ((a & 1) == 0 && (b & 1) == 0 ) {
-                ans = 2 * ans + c;
+                ans = ans << 1;
+                ans = ans | c;
                 c = 0;
             } else if ( (a & 1) == 0 && ( b & 1 ) == 1 ) {
                 if (c == 1) {
-                    ans = 2 * ans + 0;
+                    ans = ans << 1;
                     c = 1;
                 } else {
-                    ans = 2 * ans + 1;
+                    ans = ans << 1;
+                    ans = ans | 1;
                 }
             } else if ( (a & 1) == 1 && ( b & 1 ) == 0) {
                 if (c == 1) {
-                    ans = 2 * ans + 0;
+                    ans = ans << 1;
                     c = 1;
                 } else {
-                    ans = 2 * ans + 1;
+                    ans = ans << 1;
+                    ans = ans | 1;
                 }
             } else if ( (a & 1) == 1 && ( b & 1 ) == 1) {
                 if (c == 1) {
-                    ans = 2 * ans + 1;
+                    ans = ans << 1;
+                    ans = ans | 1;
                 } else {
-                    ans = 2 * ans + 0;
+                    ans = ans << 1;
                 }
                 c = 1;
             }
