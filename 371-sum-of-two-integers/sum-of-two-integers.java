66278abebb5ab5e0bd82a9ff69b29ed25c0117c1
Hemant Parihar <hemant.parihar264@gmail.com>
2024-11-07 19:46:06 +0530

    Time: 0 ms (100.00%) | Memory: 40.6 MB (17.92%) - LeetSync

---
+++ 371-sum-of-two-integers/sum-of-two-integers.java
@@ -0,0 +1,45 @@
+class Solution {
+    public int getSum(int a, int b) {
+        int ans = 0;
+        int c = 0;
+        for(int i = 0; i < 32; i++) {
+            if ((a & 1) == 0 && (b & 1) == 0 ) {
+                ans = 2 * ans + c;
+                c = 0;
+            } else if ( (a & 1) == 0 && ( b & 1 ) == 1 ) {
+                if (c == 1) {
+                    ans = 2 * ans + 0;
+                    c = 1;
+                } else {
+                    ans = 2 * ans + 1;
+                }
+            } else if ( (a & 1) == 1 && ( b & 1 ) == 0) {
+                if (c == 1) {
+                    ans = 2 * ans + 0;
+                    c = 1;
+                } else {
+                    ans = 2 * ans + 1;
+                }
+            } else if ( (a & 1) == 1 && ( b & 1 ) == 1) {
+                if (c == 1) {
+                    ans = 2 * ans + 1;
+                } else {
+                    ans = 2 * ans + 0;
+                }
+                c = 1;
+            }
+            a = a >> 1;
+            b = b >> 1;
+        }
+
+        // System.out.println(ans);
+        
+        int ret = 0;
+        for(int i = 0; i < 32; i++) {
+            ret = ret * 2 + (ans & 1);
+            ans = ans >> 1;
+        }
+
+        return ret;
+    }
+}
