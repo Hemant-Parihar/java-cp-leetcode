3500e5bff8ec82e5f933d530da29cdd2fc2f15e0
Hemant Parihar <hemant.parihar264@gmail.com>
2024-11-11 22:59:47 +0530

    Time: 669 ms (23.83%) | Memory: 270.5 MB (6.18%) - LeetSync

--- 212-word-search-ii/word-search-ii.java
+++ 212-word-search-ii/word-search-ii.java
@@ -1,43 +1,69 @@
-
 class Solution {
 
-    HashSet<String> set = new HashSet<>();
-    HashSet<String> ansSet = new HashSet<>();
+    class Node {
+        char ch;
+        Node[] next = new Node[26];
+        Node(char val) {
+            ch = val;
+        }
+    }
 
     public List<String> findWords(char[][] board, String[] words) {
-        for(int i = 0; i < words.length; i++) {
-            set.add(words[i]);
-        }
-        for(int i = 0; i < board.length; i++) {
-            for(int j = 0; j < board[0].length; j++) {
-                dfs(i, j, board, new StringBuilder());
+        int m = board.length;
+        int n = board[0].length;
+
+        Node root = new Node('.');
+
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                buildTrieDFS(i, j, root, board, 0);
             }
         }
-        ArrayList<String> list = new ArrayList<>();
-        list.addAll(ansSet);
-        return list;
-    }
 
-    void dfs(int i, int j, char[][] board, StringBuilder str) {
-        if (i < 0 || j < 0 || i >= board.length || j >= board[0].length
-            || board[i][j] == '.' || str.length() > 10) return;
+        List<String> ans = new ArrayList<>();
 
-        char ch = board[i][j];
-        str.append(ch);
-        board[i][j] = '.';
-        
-        String s = str.toString();
-        if ( set.contains(s) ) {
-            ansSet.add(s);
+        for(int i = 0; i < words.length; i++) {
+            String word = words[i];
+            if (search(word, root)) {
+                ans.add(word);
+            }
         }
 
-        dfs(i + 1, j, board, str);
-        dfs(i - 1, j, board, str);
-        dfs(i, j + 1, board, str);
-        dfs(i, j - 1, board, str);
-
-        str.deleteCharAt(str.length() - 1);
-        board[i][j] = ch;
+        return ans;
     }
 
+    void printTrie(Node ptr) {
+        
+    }
+
+    boolean search(String word, Node ptr) {
+        
+        int i = 0;
+        while(i < word.length()) {
+            char ch = word.charAt(i);
+            if (ptr.next[ch - 'a'] == null) return false;
+            ptr = ptr.next[ch - 'a'];
+            i++;
+        }
+        return true;
+    }
+
+    void buildTrieDFS(int i, int j, Node ptr, char[][] board, int len) {
+        if (i < 0 || j < 0 || i >= board.length || j >= board[0].length || len > 10 || board[i][j] == '.') return;
+
+        char ch = board[i][j];
+        board[i][j] = '.';
+        
+        if (ptr.next[ch - 'a'] == null) {
+            ptr.next[ch - 'a'] = new Node(ch);
+        }
+        ptr = ptr.next[ch - 'a'];
+
+        buildTrieDFS(i + 1, j, ptr, board, len + 1);
+        buildTrieDFS(i - 1, j, ptr, board, len + 1);
+        buildTrieDFS(i, j - 1, ptr, board, len + 1);
+        buildTrieDFS(i, j + 1, ptr, board, len + 1);
+
+        board[i][j] = ch;
+    }
 }
