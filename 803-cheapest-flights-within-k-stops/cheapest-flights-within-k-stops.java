ea5d60dffc7dde999e0f9df14f3a22238a672de7
Hemant Parihar <hemant.parihar264@gmail.com>
2024-11-17 16:31:46 +0530

    Time: 8 ms (39.23%) | Memory: 44.3 MB (49.03%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.java
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.java
@@ -1,50 +1,66 @@
 class Solution {
+    class Edge {
+        int v;
+        int w;
+        Edge(int v, int w) {
+            this.v = v;
+            this.w = w;
+        }
+    }
+
+    class Node{
+        int u;
+        int cost;
+        int len;
+        Node(int u, int cost, int len) {
+            this.u = u;
+            this.cost = cost;
+            this.len = len;
+        }
+    }
+
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
-        ArrayList[] adjList = new ArrayList[n];
+        ArrayList[] graph = new ArrayList[n];
         for(int i = 0; i < n; i++) {
-            adjList[i] = new ArrayList<Pair<Integer, Integer>>();
+            graph[i] = new ArrayList<Edge>();
         }
+
         for(int i = 0; i < flights.length; i++) {
-            adjList[flights[i][0]].add(new Pair(flights[i][1], flights[i][2]));
+            int u = flights[i][0];
+            int v = flights[i][1];
+            int w = flights[i][2];
+
+            graph[u].add(new Edge(v, w));
         }
 
-        // System.out.println(Arrays.toString(adjList));
+        PriorityQueue<Node> pq = new PriorityQueue<>( (a, b) -> a.len - b.len );
 
-        int[][] visited = new int[n][k + 2];
-        for(int[] row : visited) {
-            Arrays.fill(row, Integer.MAX_VALUE);
-        }
+        int[] ans = new int[n];
+        Arrays.fill(ans, Integer.MAX_VALUE);
 
-        PriorityQueue<int[]> heap = new PriorityQueue<>( (a, b) -> a[2] - b[2]);
-        heap.add(new int[]{src, 0, k + 1});
-        visited[src][k + 1] = 0;
+        ans[src] = 0;
+        pq.add(new Node(src, 0, 0));
 
-        while(!heap.isEmpty()) {
-            int[] arr = heap.poll();
-            int start = arr[0];
-            int cost = arr[1];
-            int step = arr[2];
+        while( !pq.isEmpty() ) {
 
-            if (step == 0 || start == dst) {
-                continue;
-            }
+            Node node = pq.poll();
+            int u = node.u;
 
-            step--;
-            for(int i = 0; i < adjList[start].size(); i++) {
-                Pair<Integer, Integer> pair = (Pair<Integer, Integer>)adjList[start].get(i);
-                int end = pair.getKey();
-                int dis = pair.getValue();
-                if (visited[end][step] > dis + cost) {
-                    visited[end][step] = dis + cost;
-                    heap.add(new int[]{end, dis + cost, step});
+            if (node.len == k + 1) continue;
+            
+            for(int i = 0; i < graph[u].size(); i++) {
+                Edge edge = (Edge) graph[u].get(i);
+                if ( (node.cost + edge.w) < ans[edge.v] ) {
+                    ans[edge.v] = node.cost + edge.w;
+                    pq.add( new Node(edge.v, node.cost + edge.w, node.len + 1) );
                 }
             }
-        }
-        int ans = Integer.MAX_VALUE;
-        for(int i = 0; i <= k + 1; i++) {
-            ans = Math.min(ans, visited[dst][i]);
+
         }
 
-        return ans == Integer.MAX_VALUE ? -1 : ans;
+        System.out.println(Arrays.toString(ans));
+
+        if (ans[dst] == Integer.MAX_VALUE) return -1;
+        return ans[dst];
     }
 }
