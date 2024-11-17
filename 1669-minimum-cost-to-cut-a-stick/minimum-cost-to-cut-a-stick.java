b373d72df09a7bc1bd852ddaab31ca97797c40bc
Hemant Parihar <hemant.parihar264@gmail.com>
2024-11-18 00:24:09 +0530

    Time: 680 ms (5.04%) | Memory: 48.6 MB (5.02%) - LeetSync

--- 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.java
+++ 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.java
@@ -1,18 +1,19 @@
 class Solution {
     public int minCost(int n, int[] cuts) {
 
-        HashMap<Pair<Integer, Integer>, Integer> map = new HashMap<>();
+        HashMap<String, Integer> map = new HashMap<>();
         return solve(0, n, cuts, map);
     }
 
-    int solve(int i, int j, int[] cuts, HashMap<Pair<Integer, Integer>, Integer> map) {
+    int solve(int i, int j, int[] cuts, HashMap<String, Integer> map) {
         if (j - i <= 1)
             return 0;
 
-        Pair pair = new Pair(i, j);
-        if ( map.containsKey(pair) ) return map.get(pair);
+        String str = i + "-" + j;
+        if ( map.containsKey(str) ) return map.get(str);
 
         int ans = Integer.MAX_VALUE;
+
         for(int k = 0; k < cuts.length; k++) {
             if (cuts[k] > i && cuts[k] < j) {
                 int temp_ans = (j - i) + solve(i, cuts[k], cuts, map) + solve(cuts[k], j, cuts, map);
@@ -23,7 +24,7 @@ class Solution {
         if (ans == Integer.MAX_VALUE) {
             ans = 0;
         }
-        map.put(pair, ans);
+        map.put(str, ans);
         return ans;
     }
 }
