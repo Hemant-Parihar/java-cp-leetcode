a8a82fa2b10551fa10ebf9b31bc91376fbcd5829
Hemant Parihar <hemant.parihar264@gmail.com>
2024-11-22 22:14:31 +0530

    Time: 340 ms (5.01%) | Memory: 45.6 MB (37.84%) - LeetSync

--- 135-candy/candy.java
+++ 135-candy/candy.java
@@ -1,25 +1,38 @@
 class Solution {
     public int candy(int[] ratings) {
         int n = ratings.length;
-        if (n == 1) return 1;
         int[] candy = new int[n];
         candy[0] = 1;
         for(int i = 1; i < n; i++) {
-            if (ratings[i -1] < ratings[i]) {
-                candy[i] = candy[i - 1] + 1;
-            } else {
+            if (ratings[i] < ratings[i - 1]) {
+                // (i - 1) should get more candy.
                 candy[i] = 1;
+
                 int j = i - 1;
-                while(j >= 0 && ratings[j] > ratings[j+ 1] && candy[j] < candy[j+1] + 1) {
-                    candy[j] = candy[j+ 1] + 1;
-                    j--;
+                while(j >= 0 && ratings[j + 1] < ratings[j]) {
+                    if ( candy[j + 1] == candy[j]) {
+                        candy[j]++;
+                        j--;
+                    } else {
+                        break;
+                    }
                 }
+
+            } else if (ratings[i] == ratings[i - 1]) {
+                candy[i] = 1;
+            } else {
+                candy[i] = candy[i-1] + 1;
             }
+
         }
-        int ans = 0;
+
+        System.out.println(Arrays.toString(candy));
+
+        int sum = 0;
         for(int i = 0; i < n; i++) {
-            ans += candy[i];
+            sum += candy[i];
         }
-        return ans;
+
+        return sum;
     }
 }
