84b6ca9382859a5c5d02cb55075ef271386b30e8
Hemant Parihar <hemant.parihar264@gmail.com>
2024-11-22 21:00:01 +0530

    Time: 70 ms (15.89%) | Memory: 41.2 MB (19.15%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.java
+++ 486-predict-the-winner/predict-the-winner.java
@@ -5,16 +5,9 @@ class Solution {
         for(int i = 0; i < n; i++) {
             tsum += nums[i];
         }
-        int[][][] dp = new int[n][n][2];
-        for(int i = 0; i < n; i++) {
-            for(int j = i; j < n; j++) {
-                for(int k = 0; k < 2; k++) {
-                    dp[i][j][k] = -1;
-                }
-            }
-        }
-        int val = solve(0, n - 1, nums, dp);
-        System.out.println(val);
+        
+        int val = solve(0, n - 1, nums);
+
         if (tsum % 2 == 1) {
             if (val > tsum / 2) return true;
         } else {
@@ -23,22 +16,19 @@ class Solution {
         return false;
     }
 
-    int solve(int i, int j, int[] piles, int[][][] dp) {
+    int solve(int i, int j, int[] piles) {
         if (i > j) return 0;
         if (i == j) {
             return piles[i];
         }
 
-        // if (dp[i][j][turn] != -1) return dp[i][j][turn];
-
 
         int ans = 0;
         ans = Math.max(
-                    piles[i] + Math.min( solve(i + 1, j - 1, piles, dp), solve(i + 2, j, piles, dp) ), 
-                    piles[j] + Math.min( solve(i + 1, j - 1, piles, dp), solve(i, j - 2, piles, dp) )
+                    piles[i] + Math.min( solve(i + 1, j - 1, piles), solve(i + 2, j, piles) ), 
+                    piles[j] + Math.min( solve(i + 1, j - 1, piles), solve(i, j - 2, piles) )
                 );
 
-        // dp[i][j][turn] = ans;
         return ans;
     }
 }
