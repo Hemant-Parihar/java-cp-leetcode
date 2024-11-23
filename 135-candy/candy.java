5a8c7f84114bcc1cb43ce43b3a4227d6ce00bbb2
Hemant Parihar <hemant.parihar264@gmail.com>
2024-11-24 00:54:07 +0530

    Time: 3 ms (83.16%) | Memory: 45 MB (94.76%) - LeetSync

--- 135-candy/candy.java
+++ 135-candy/candy.java
@@ -1,38 +1,34 @@
 class Solution {
     public int candy(int[] ratings) {
         int n = ratings.length;
-        int[] candy = new int[n];
-        candy[0] = 1;
+
+        int[] front = new int[n];
+
+        front[0] = 1;
         for(int i = 1; i < n; i++) {
-            if (ratings[i] < ratings[i - 1]) {
-                // (i - 1) should get more candy.
-                candy[i] = 1;
-
-                int j = i - 1;
-                while(j >= 0 && ratings[j + 1] < ratings[j]) {
-                    if ( candy[j + 1] == candy[j]) {
-                        candy[j]++;
-                        j--;
-                    } else {
-                        break;
-                    }
-                }
-
-            } else if (ratings[i] == ratings[i - 1]) {
-                candy[i] = 1;
+            if (ratings[i] > ratings[i - 1]) {
+                front[i] = 1 + front[i - 1];
             } else {
-                candy[i] = candy[i-1] + 1;
+                front[i] = 1;
             }
-
         }
 
-        // System.out.println(Arrays.toString(candy));
+        int[] back = new int[n];
+        back[n - 1] = 1;
+        for(int j = n - 2; j >= 0; j--) {
+            if (ratings[j] > ratings[j + 1]) {
+                back[j] = 1 + back[j + 1];
+            } else {
+                back[j] = 1;
+            }
+        }
 
-        int sum = 0;
+        int ans = 0;
+
         for(int i = 0; i < n; i++) {
-            sum += candy[i];
+            ans += Math.max(front[i], back[i]);
         }
 
-        return sum;
+        return ans;
     }
 }
