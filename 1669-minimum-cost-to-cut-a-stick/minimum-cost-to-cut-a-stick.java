6a0cd0b4df17892ad77b2237fcf64fcdf59227aa
Hemant Parihar <hemant.parihar264@gmail.com>
2025-03-15 15:53:22 +0530

    Time: 23 ms (53.09%) | Memory: 44.7 MB (7.27%) - LeetSync

--- 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.java
+++ 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.java
@@ -1,42 +1,32 @@
 class Solution {
-
-    int[][] dp = new int[102][102];
-
     public int minCost(int n, int[] cuts) {
-        var c = new ArrayList<Integer>();
-        for (int cut : cuts)
-            c.add(cut);
-        c.addAll(Arrays.asList(0, n));
-        Collections.sort(c);
-
-        for(int i = 0; i < dp.length; i++) {
-            for(int j = 0; j < dp.length; j++) {
-                dp[i][j] = -1;
-            }
+        int[][] dp = new int[103][103];
+        for(int i = 0; i < 101; i++) {
+            Arrays.fill(dp[i], -1);
         }
-        
-        return solve(c, 0, c.size() - 1, dp);
+        Arrays.sort(cuts);
+        int[] arr = new int[cuts.length + 2];
+        arr[0] = 0;
+        arr[cuts.length + 1] = n;
+        for(int i = 0; i < cuts.length; i++) {
+            arr[i + 1] = cuts[i];
+        }
+        return solve(0, arr.length - 1, arr, dp);
     }
 
-    int solve(ArrayList<Integer> cuts, int i, int j, int[][] dp) {
-        if (j - i < 0)
-            return 0;
+    int solve(int i, int j, int[] cuts, int[][] dp) {
+        int ans = Integer.MAX_VALUE;
+        boolean f = false;
 
         if (dp[i][j] != -1) return dp[i][j];
 
-        int ans = Integer.MAX_VALUE;
-
-        for(int k = i + 1; k <= j - 1; k++) {
-            int temp_ans = ( (cuts.get(j) - cuts.get(i)) + solve(cuts, i, k, dp)
-                             + solve(cuts, k, j, dp) );
+        for(int k = i + 1; k < j; k++) {
+            f = true;
+            int temp_ans = (cuts[j] - cuts[i]) + solve(i, k, cuts, dp) + solve(k, j, cuts, dp);
             ans = Math.min(ans, temp_ans);
         }
 
-        if (ans == Integer.MAX_VALUE) {
-            ans = 0;
-        }
-
-        dp[i][j] = ans;
-        return ans;
+        dp[i][j] = f ? ans : 0;
+        return dp[i][j];
     }
 }
