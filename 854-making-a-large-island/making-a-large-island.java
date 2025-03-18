71b865b9a97e29c667b6988e81177da57c29037b
Hemant Parihar <hemant.parihar264@gmail.com>
2025-03-18 23:53:42 +0530

    Time: 128 ms (25.91%) | Memory: 74.2 MB (68.34%) - LeetSync

--- 854-making-a-large-island/making-a-large-island.java
+++ 854-making-a-large-island/making-a-large-island.java
@@ -19,7 +19,7 @@ class Solution {
             }
         }
 
-        System.out.println(areaIndex);
+        // System.out.println(areaIndex);
 
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < n; j++) {
