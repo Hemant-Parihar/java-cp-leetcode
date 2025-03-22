da188a373006e456ed0aedd0180ea35202271035
Hemant Parihar <hemant.parihar264@gmail.com>
2025-03-22 20:59:31 +0530

    Time: 22 ms (51.03%) | Memory: 45.1 MB (70.39%) - LeetSync

--- 1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/minimum-cost-to-make-at-least-one-valid-path-in-a-grid.java
+++ 1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/minimum-cost-to-make-at-least-one-valid-path-in-a-grid.java
@@ -1,109 +1,28 @@
 class Solution {
-
-    class Node {
-        int x, y, dis;
-        Node(int x, int y, int cost) {
-            this.x = x;
-            this.y = y;
-            this.dis = cost;
-        }
-        @Override
-        public boolean equals(Object obj) {
-            if (this == obj) return true; // Check if both references are the same
-            if (obj == null || getClass() != obj.getClass()) return false; // Ensure obj is a Node
-
-            Node node = (Node) obj; // Typecast obj to Node
-            return this.x == node.x && this.y == node.y && this.dis == node.dis;
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(x, y, dis);
-        }
-    }
-
+    int[][] DIR = new int[][]{{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
     public int minCost(int[][] grid) {
-        int m = grid.length;
-        int n = grid[0].length;
-
-        PriorityQueue<Node> pq = new PriorityQueue<>( (a, b) -> a.dis - b.dis );
-        HashSet<Node> set = new HashSet<>();
-
-        Node newNode = new Node(0, 0, 0);
-        pq.add(newNode);
-        
-
-        while(!pq.isEmpty()) {
-            Node node = pq.poll();
-
-            if (node.x == m - 1 && node.y == n - 1) return node.dis;
-
-            if (node.x - 1 >= 0) {
-                if (grid[node.x][node.y] == 4) {
-                    Node next = new Node(node.x - 1, node.y, node.dis);
-                    if (!set.contains(next)) {
-                        pq.add( next );
-                        set.add(next);
-                    }
-                } else {
-                    Node next = new Node(node.x - 1, node.y, node.dis + 1);
-                    if (!set.contains(next)) {
-                        pq.add( next );
-                        set.add(next);
+        int m = grid.length, n = grid[0].length;
+        PriorityQueue<int[]> q = new PriorityQueue<>((o1, o2) -> o1[0] - o2[0]); // minHeap by cost
+        q.offer(new int[]{0, 0, 0});
+        int[][] dist = new int[m][n];
+        for (int i = 0; i < m; i++) Arrays.fill(dist[i], Integer.MAX_VALUE);
+        dist[0][0] = 0;
+        while (!q.isEmpty()) {
+            int[] top = q.poll();
+            int cost = top[0], r = top[1], c = top[2];
+            if (dist[r][c] != cost) continue; // avoid outdated (dist[r,c], r, c) to traverse neighbors again!
+            for (int i = 0; i < 4; i++) {
+                int nr = r + DIR[i][0], nc = c + DIR[i][1];
+                if (nr >= 0 && nr < m && nc >= 0 && nc < n) {
+                    int ncost = cost;
+                    if (i != (grid[r][c] - 1)) ncost += 1; // change direction -> ncost = cost + 1
+                    if (dist[nr][nc] > ncost) {
+                        dist[nr][nc] = ncost;
+                        q.offer(new int[]{ncost, nr, nc});
                     }
                 }
             }
-
-            if (node.x + 1 < m) {
-                if (grid[node.x][node.y] == 3) {
-                    Node next = new Node(node.x + 1, node.y, node.dis);
-                    if (!set.contains(next)) {
-                        pq.add( next );
-                        set.add(next);
-                    }
-                } else {
-                    Node next = new Node(node.x + 1, node.y, node.dis + 1);
-                    if (!set.contains(next)) {
-                        pq.add( next );
-                        set.add(next);
-                    }
-                }
-            }
-
-            if (node.y - 1 >= 0) {
-                if (grid[node.x][node.y] == 2) {
-                    Node next = new Node(node.x, node.y - 1, node.dis);
-                    if (!set.contains(next)) {
-                        pq.add( next );
-                        set.add(next);
-                    }
-                } else {
-                    Node next = new Node(node.x, node.y - 1, node.dis + 1);
-                    if (!set.contains(next)) {
-                        pq.add( next );
-                        set.add(next);
-                    }
-                }
-            }
-
-            if (node.y + 1 < n) {
-                if (grid[node.x][node.y] == 1) {
-                    Node next = new Node(node.x, node.y + 1, node.dis);
-                    if (!set.contains(next)) {
-                        pq.add( next );
-                        set.add(next);
-                    }
-                } else {
-                    Node next = new Node(node.x, node.y + 1, node.dis + 1);
-                    if (!set.contains(next)) {
-                        pq.add( next );
-                        set.add(next);
-                    }
-                }
-            }
-
         }
-
-        return -1;
+        return dist[m - 1][n - 1];
     }
 }
