04b7e3701defe23069d00dc59d3a1fd21946c608
Hemant Parihar <hemant.parihar264@gmail.com>
2025-03-22 20:57:37 +0530

    Time: 1818 ms (5.10%) | Memory: 180.9 MB (5.03%) - LeetSync

--- 1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/minimum-cost-to-make-at-least-one-valid-path-in-a-grid.java
+++ 1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/minimum-cost-to-make-at-least-one-valid-path-in-a-grid.java
@@ -1,44 +1,109 @@
 class Solution {
-    
+
+    class Node {
+        int x, y, dis;
+        Node(int x, int y, int cost) {
+            this.x = x;
+            this.y = y;
+            this.dis = cost;
+        }
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) return true; // Check if both references are the same
+            if (obj == null || getClass() != obj.getClass()) return false; // Ensure obj is a Node
+
+            Node node = (Node) obj; // Typecast obj to Node
+            return this.x == node.x && this.y == node.y && this.dis == node.dis;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(x, y, dis);
+        }
+    }
+
     public int minCost(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
-        int[][] ans = new int[m][n];
-        for(int i = 0; i < m; i++) {
-            Arrays.fill(ans[i], Integer.MAX_VALUE);
-        }
-        solve(0, 0, 0, grid, ans);
-        return ans[m - 1][n - 1];
-    }
 
-    void solve(int i, int j, int cost, int[][] grid, int[][] ans) {
-        if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || ans[i][j] <= cost) {
-            return;
-        }
-        if (cost - 2 >= (grid.length + grid[0].length)) return;
+        PriorityQueue<Node> pq = new PriorityQueue<>( (a, b) -> a.dis - b.dis );
+        HashSet<Node> set = new HashSet<>();
 
-        ans[i][j] = cost;
-        if (grid[i][j] == 1) {
-            solve(i + 1, j, cost + 1, grid, ans);
-            solve(i - 1, j, cost + 1, grid, ans);
-            solve(i, j + 1, cost, grid, ans);
-            solve(i, j - 1, cost + 1, grid, ans);
-        } else if (grid[i][j] == 2) {
-            solve(i + 1, j, cost + 1, grid, ans);
-            solve(i - 1, j, cost + 1, grid, ans);
-            solve(i, j + 1, cost + 1, grid, ans);
-            solve(i, j - 1, cost, grid, ans);
-        } else if (grid[i][j] == 3) {
-            solve(i + 1, j, cost, grid, ans);
-            solve(i - 1, j, cost + 1, grid, ans);
-            solve(i, j + 1, cost + 1, grid, ans);
-            solve(i, j - 1, cost + 1, grid, ans);
-        } else {
-            solve(i + 1, j, cost + 1, grid, ans);
-            solve(i - 1, j, cost, grid, ans);
-            solve(i, j + 1, cost + 1, grid, ans);
-            solve(i, j - 1, cost + 1, grid, ans);
-        }
+        Node newNode = new Node(0, 0, 0);
+        pq.add(newNode);
         
+
+        while(!pq.isEmpty()) {
+            Node node = pq.poll();
+
+            if (node.x == m - 1 && node.y == n - 1) return node.dis;
+
+            if (node.x - 1 >= 0) {
+                if (grid[node.x][node.y] == 4) {
+                    Node next = new Node(node.x - 1, node.y, node.dis);
+                    if (!set.contains(next)) {
+                        pq.add( next );
+                        set.add(next);
+                    }
+                } else {
+                    Node next = new Node(node.x - 1, node.y, node.dis + 1);
+                    if (!set.contains(next)) {
+                        pq.add( next );
+                        set.add(next);
+                    }
+                }
+            }
+
+            if (node.x + 1 < m) {
+                if (grid[node.x][node.y] == 3) {
+                    Node next = new Node(node.x + 1, node.y, node.dis);
+                    if (!set.contains(next)) {
+                        pq.add( next );
+                        set.add(next);
+                    }
+                } else {
+                    Node next = new Node(node.x + 1, node.y, node.dis + 1);
+                    if (!set.contains(next)) {
+                        pq.add( next );
+                        set.add(next);
+                    }
+                }
+            }
+
+            if (node.y - 1 >= 0) {
+                if (grid[node.x][node.y] == 2) {
+                    Node next = new Node(node.x, node.y - 1, node.dis);
+                    if (!set.contains(next)) {
+                        pq.add( next );
+                        set.add(next);
+                    }
+                } else {
+                    Node next = new Node(node.x, node.y - 1, node.dis + 1);
+                    if (!set.contains(next)) {
+                        pq.add( next );
+                        set.add(next);
+                    }
+                }
+            }
+
+            if (node.y + 1 < n) {
+                if (grid[node.x][node.y] == 1) {
+                    Node next = new Node(node.x, node.y + 1, node.dis);
+                    if (!set.contains(next)) {
+                        pq.add( next );
+                        set.add(next);
+                    }
+                } else {
+                    Node next = new Node(node.x, node.y + 1, node.dis + 1);
+                    if (!set.contains(next)) {
+                        pq.add( next );
+                        set.add(next);
+                    }
+                }
+            }
+
+        }
+
+        return -1;
     }
 }
