0ba3b2f6acd61cc5752c8bd4c48104be00b5b76e
Hemant Parihar <hemant.parihar264@gmail.com>
2025-04-11 00:21:41 +0530

    Time: 130 ms (12.42%) | Memory: 122.3 MB (47.35%) - LeetSync

---
+++ 3657-check-if-grid-can-be-cut-into-sections/check-if-grid-can-be-cut-into-sections.java
@@ -0,0 +1,42 @@
+class Solution {
+    public boolean checkValidCuts(int n, int[][] rec) {
+        int size = rec.length;
+        int[][] l1 = new int[size][2];
+        int[][] l2 = new int[size][2];
+
+        for(int i = 0; i < size; i++) {
+            l1[i][0] = rec[i][0];
+            l1[i][1] = rec[i][2];
+
+            l2[i][0] = rec[i][1];
+            l2[i][1] = rec[i][3];
+        }
+
+        Arrays.sort(l1, (a, b) -> Integer.compare(a[0], b[0]));
+        Arrays.sort(l2, (a, b) -> Integer.compare(a[0], b[0]));
+
+        int count1 = 0;
+        int end = l1[0][1];
+        int count2 = 0;
+        for(int i = 0; i < size - 1; i++) {
+            if (end <= l1[i + 1][0]) {
+                count1++;
+            }
+            end = Math.max(end, l1[i + 1][1]);
+        }
+
+        if (count1 > 1) {
+            return true;
+        }
+
+        end = l2[0][1];
+        for(int i = 0; i < size - 1; i++) {
+            if (end <= l2[i + 1][0]) {
+                count2++;
+            }
+            end = Math.max(end, l2[i + 1][1]);
+        }
+        if (count2 > 1) return true;
+        return false;
+    }
+}
