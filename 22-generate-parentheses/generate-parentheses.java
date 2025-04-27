0bdb4f497edafd22866f0372264435384d68afb4
Hemant Parihar <hemant.parihar264@gmail.com>
2025-04-27 21:56:48 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (97.46%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.java
+++ 22-generate-parentheses/generate-parentheses.java
@@ -1,39 +1,25 @@
 class Solution {
-    
-    List<String> ans = new ArrayList<>();
-    int N;
-
     public List<String> generateParenthesis(int n) {
-        N = n;
-        solve(0, 0, new StringBuilder());
+        List<String> ans = new ArrayList<>();
+        solve(n, 0, new StringBuilder(), ans);
         return ans;
     }
 
-    void solve(int open, int close, StringBuilder str) {
-        if (close > open) return;
-        
-        if (open == N) {
-            int temp = close;
-            while(temp < N) {
-                temp++;
-                str.append(')');
-            }
+    void solve(int n, int count, StringBuilder str, List<String> ans) {
+        if (n == 0 && count == 0) {
             ans.add(str.toString());
-
-            while(temp > close) {
-                temp--;
-                str.deleteCharAt( str.length() - 1 );
-            }
             return;
         }
 
-        str.append('(');
-        solve(open + 1, close, str);
+        if (n > 0) {
+            solve(n - 1, count + 1, str.append('('), ans);
+            str.deleteCharAt(str.length() - 1);
+        }
 
-        str.deleteCharAt( str.length() - 1 );
+        if (count > 0) {
+            solve(n, count - 1, str.append(')'), ans);
+            str.deleteCharAt(str.length() - 1);
+        }
 
-        str.append(')');
-        solve(open, close + 1, str);
-        str.deleteCharAt( str.length() - 1 );
     }
 }
