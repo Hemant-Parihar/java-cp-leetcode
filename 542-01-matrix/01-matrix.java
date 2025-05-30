280d2d44619d93c83c0442e4081975cf7cb955e0
Hemant Parihar <hemant.parihar264@gmail.com>
2025-05-30 17:31:24 +0530

    Time: 35 ms (5.23%) | Memory: 47.8 MB (30.89%) - LeetSync

---
+++ 542-01-matrix/01-matrix.java
@@ -0,0 +1,52 @@
+class Solution {
+    public int[][] updateMatrix(int[][] mat) {
+        int m = mat.length;
+        int n = mat[0].length;
+
+        PriorityQueue<int[]> pq = new PriorityQueue<>( (a, b) -> Integer.compare(a[0], b[0]) );
+        int[][] dis = new int[m][n];
+
+        for(int i = 0; i < m; i++) {
+            Arrays.fill(dis[i], Integer.MAX_VALUE);
+        }
+
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if (mat[i][j] == 0) {
+                    pq.add(new int[]{0, i, j});
+                    dis[i][j] = 0;
+                }
+            }
+        }
+
+        while(!pq.isEmpty()) {
+            int[] node = (int[]) pq.poll();
+            int d = node[0];
+            int i = node[1];
+            int j = node[2];
+
+            if (i + 1 < m && dis[i + 1][j] > (d + 1)) {
+                dis[i + 1][j] = d + 1;
+                pq.add(new int[]{d + 1, i + 1, j});
+            }
+
+            if (i - 1 >= 0 && dis[i - 1][j] > (d + 1)) {
+                dis[i - 1][j] = d + 1;
+                pq.add(new int[]{d + 1, i - 1, j});
+            }
+
+            if (j + 1 < n && dis[i][j + 1] > (d + 1)) {
+                dis[i][j + 1] = d + 1;
+                pq.add(new int[]{d + 1, i, j + 1});
+            }
+
+            if (j - 1 >= 0 && dis[i][j - 1] > (d + 1)) {
+                dis[i][j - 1] = d + 1;
+                pq.add(new int[]{d + 1, i, j - 1});
+            }
+
+        }
+
+        return dis;
+    }
+}
