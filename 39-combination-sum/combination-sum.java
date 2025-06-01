174de454794e71212b439e254b3d2a09b2adb56e
Hemant Parihar <hemant.parihar264@gmail.com>
2025-06-01 19:45:39 +0530

    Time: 3 ms (23.25%) | Memory: 44.6 MB (64.13%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -1,26 +1,29 @@
 class Solution {
-    List<List<Integer>> ans = new ArrayList<>();
     public List<List<Integer>> combinationSum(int[] can, int target) {
-        solve(0, can, target, new ArrayList<>());
-        return ans;
+        HashSet<List<Integer>> ans = new HashSet<>();
+        solve(0, can, target, ans, new ArrayList<>());
+        List<List<Integer>> ret = new ArrayList<>();
+        for(List<Integer> list : ans) {
+            ret.add(list);
+        }
+        return ret;
     }
 
-    void solve(int i, int[] can, int target, List<Integer> list) {
+    void solve(int i, int[] can, int target, HashSet<List<Integer>> ans, List<Integer> list) {
+        int n = can.length;
         if (target == 0) {
-            ans.add(list);
+            ans.add(new ArrayList<>(list));
             return;
         }
-        if (i >= can.length) return;
+        if (i == n) return;
 
-        if (target - can[i] >= 0) {
-            List<Integer> newList = new ArrayList<>();
-            for(int k = 0; k < list.size(); k++) {
-                newList.add(list.get(k));
-            }
-            newList.add(can[i]);
-            solve(i, can, target - can[i], newList);
+        if (target >= can[i]) {
+            list.add(can[i]);
+            solve(i, can, target - can[i], ans, list);
+            list.remove(list.size() - 1);
         }
 
-        solve(i + 1, can, target, list);
+        solve(i + 1, can, target, ans, list);
+
     }
 }
