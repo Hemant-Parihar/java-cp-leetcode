a319cf9957755f7ab81413d94b34504609a38ac9
Hemant Parihar <hemant.parihar264@gmail.com>
2025-06-03 00:18:15 +0530

    Time: 101 ms (19.17%) | Memory: 45.7 MB (20.94%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -1,14 +1,10 @@
 class Solution {
     public String minWindow(String s, String t) {
         int m = t.length();
-        HashMap<Character, Integer> map = new HashMap<>();
+        int[] arr = new int[128];
         for(int i = 0; i < m; i++) {
             char ch = t.charAt(i);
-            if (map.containsKey(ch)) {
-                map.put(ch, map.get(ch) + 1);
-            } else {
-                map.put(ch, 1);
-            }
+            arr[ch]++;
         }
 
         int count = 0;
@@ -19,25 +15,21 @@ class Solution {
 
         for(j = 0; j < s.length(); j++) {
             char ch = s.charAt(j);
-            if (map.containsKey(ch)) {
-                if (map.get(ch) > 0) {
-                    count++;
+            if (arr[ch] > 0) {
+                count++;
+            }
+            arr[ch]--;
+            while (count == m) {
+                if (len > (j - i + 1)) {
+                    len = j - i + 1;
+                    ans = s.substring(i, j + 1);
                 }
-                map.put(ch, map.get(ch) - 1);
-                while (count == m) {
-                    if (len > (j - i + 1)) {
-                        len = j - i + 1;
-                        ans = s.substring(i, j + 1);
-                    }
-                    char ch_p = s.charAt(i);
-                    if (map.containsKey(ch_p)) {
-                        map.put(ch_p, map.get(ch_p) + 1);
-                        if (map.get(ch_p) > 0) {
-                            count--;
-                        }
-                    }
-                    i++;
+                char ch_p = s.charAt(i);
+                arr[ch_p]++;
+                if (arr[ch_p] > 0) {
+                    count--;
                 }
+                i++;
             }
         }
 
