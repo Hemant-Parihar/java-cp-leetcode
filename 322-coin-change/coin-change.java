38b655e989379382e0c01759c221c4cb97495f2a
Hemant Parihar <hemant.parihar264@gmail.com>
2026-01-21 16:26:57 +0530

    Time: 34 ms (16.18%) | Memory: 47.7 MB (7.96%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,24 +1,26 @@
 class Solution {
 
     int INT_MAX = 100000;
-
     public int coinChange(int[] coins, int amount) {
         int n = coins.length;
-        int[][] dp = new int[n][amount + 1];
-        for(int i = 0; i < n; i++) {
-            Arrays.fill(dp[i], -1);
+
+        int dp[][] = new int[n + 1][amount + 1];
+
+        for(int i = 0; i <= n; i++) {
+            for(int j = 0; j <= amount; j++) {
+                dp[i][j] = -1;
+            }
         }
-        int val = solve(0, coins, amount, dp);
-        if (val >= INT_MAX) {
-            return -1;
-        }
+
+        int val = solve(n - 1, coins, amount, dp);
+        if (val >= INT_MAX) return -1;
         return val;
     }
 
     int solve(int i, int[] coins, int amount, int[][] dp) {
-        int n = coins.length;
+        if (i < 0) return INT_MAX;
+
         if (amount == 0) return 0;
-        if (i == n && amount != 0) return INT_MAX;
 
         if (dp[i][amount] != -1) return dp[i][amount];
 
@@ -26,7 +28,8 @@ class Solution {
         if (amount >= coins[i]) {
             p = 1 + solve(i, coins, amount - coins[i], dp);
         }
-        int up = solve(i + 1, coins, amount, dp);
+        int up = solve(i - 1, coins, amount, dp);
+
         return dp[i][amount] = Math.min(p, up);
     }
 }
